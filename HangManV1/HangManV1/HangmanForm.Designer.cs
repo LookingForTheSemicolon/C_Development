bd9e84403e056e945857c286be28c47bd742ecdc
Dennis <dennisbergfelder@hotmail.de>
2019-07-13 13:27:19 +0200

    Added current development

---
+++ HangManV1/HangManV1/HangmanForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace HangManV1
+{
+    partial class HangmanForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 604);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Hangman";
+            this.Text = "HangmanForm";
+            this.Load += new System.EventHandler(this.Hangman_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ HangManV1/HangManV1/HangmanForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace HangManV1
+{
+    public partial class HangmanForm : Form
+    {
+        public HangmanForm()
+        {
+            InitializeComponent();
+        }
+        int errorCount = 0;
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GenerateGame();
+        }
+        public void GenerateGame()
+        {
+            int hor = 30;
+            int ver = 30;
+            Button[] btnArray = new Button[26];
+            String[] btnChar = new String[26];
+            string loesungsWort = "numerisch";
+
+            StringBuilder verdecktesWort = new StringBuilder(loesungsWort);
+
+            for (int i = 0; i < verdecktesWort.Length; i++)
+            {
+                verdecktesWort[i] = '_';
+            }
+
+            Label coveredWord = new Label();
+            coveredWord.Location = new Point(130, 129);
+            coveredWord.Size = new Size(300, 100);
+            coveredWord.Text = string.Join(" ", verdecktesWort);
+
+            Label word = new Label();
+            word.Text = "Gesucht wird: ";
+            word.Location = new Point(30, 129);
+
+            Panel btnPanel = new Panel();
+            btnPanel.Location = new Point(0, 330);
+            btnPanel.Size = new Size(500, 400);
+
+            PictureBox pBox = new PictureBox();
+            pBox.Location = new Point(550, 100);
+            pBox.Image = Image.FromFile("C:/Users/Dennis/source/repos/Test/Test/Resources/" + errorCount + ".png");
+            pBox.Size = new Size(400, 400);
+
+            Controls.Add(pBox);
+            Controls.Add(word);
+            Controls.Add(btnPanel);
+            Controls.Add(coveredWord);
+
+            #region 25 characters
+            for (int i = 0, c = 97; i < btnChar.Length; i++)
+            {
+                char ascii = (char)c;
+                btnChar[i] = ascii.ToString();
+                c++;
+            }
+            #endregion
+            #region 26Buttonsa-z
+            for (int i = 0; i < btnArray.Length; i++)
+            {
+                btnArray[i] = new Button();
+                btnArray[i].Size = new Size(30, 30);
+                btnArray[i].Location = new Point(hor, ver);
+                btnArray[i].Click += new EventHandler(buttonClick);
+                if ((i + 1) % 13 != 0) { hor += 30; }
+                else { hor = 30; ver += 30; }
+                btnArray[i].Text = btnChar[i];
+                btnArray[i].Name = btnChar[i];
+                btnPanel.Controls.Add(btnArray[i]);
+            }
+            #endregion 26Buttonsa-z
+            //Click-Event for each Button
+            void buttonClick(object sender, EventArgs e)
+            {
+                Button button = sender as Button;
+                int charIndex = loesungsWort.IndexOf(button.Text);
+                if (charIndex != -1)
+                {
+                    verdecktesWort[charIndex] = button.Text[0];
+                    //button.Visible = !button.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("Buchstabe nicht vorhanden");
+                    errorCount++;
+
+                    pBox.Load("C:/Users/Dennis/source/repos/Test/Test/Resources/" + errorCount + ".png");
+                    pBox.Refresh();
+                    return;
+                }
+
+
+                coveredWord.Text = string.Join(" ", verdecktesWort);
+            }
+
+
+        }
+
+        private void Hangman_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
